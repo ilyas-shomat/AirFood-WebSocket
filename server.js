1004230a7e74a8b63b53f7d6942a652dc007fd37
ilyasshomat <ilyas.shomat@gmail.com>
2020-06-05 15:29:49 +0600

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ wss.on('connection', (ws) => {
   ws.on('close', () => console.log('Client disconnected'));
 });
 
-ws.on('message', function incoming(data) {
+wss.on('message', function incoming(data) {
   console.log(data);
 });
 
